2426d34b8ee4b562716c0d3b281bfb9bf5e0b73d
sesang06 <sesang06@naver.com>
2018-11-25 21:18:26 +0900

    Initial Commit

---
+++ iOS-todoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-todoApp
+//
+//  Created by 조세상 on 25/11/2018.
+//  Copyright © 2018 조세상. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS-todoAppTests/iOS_todoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_todoAppTests.swift
+//  iOS-todoAppTests
+//
+//  Created by 조세상 on 25/11/2018.
+//  Copyright © 2018 조세상. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_todoApp
+
+class iOS_todoAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
