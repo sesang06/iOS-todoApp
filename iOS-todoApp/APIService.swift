9a69474dd193ff600c0d87fe2c8841b6d8b170c4
sesang06 <sesang06@naver.com>
2018-12-02 17:07:34 +0900

    rxSwift Restful api 연구

---
+++ iOS-todoApp/APIService.swift
@@ -0,0 +1,77 @@
+//
+//  APIService.swift
+//  iOS-todoApp
+//
+//  Created by 조세상 on 02/12/2018.
+//  Copyright © 2018 조세상. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+public enum RequestType : String{
+    case GET, POST
+}
+protocol ApiRequest{
+    var method : RequestType { get }
+    var path : String { get }
+    var parameters : [String : String ]{ get}
+    
+}
+extension ApiRequest {
+    func request(with baseURL : URL) -> URLRequest {
+        guard var components = URLComponents(url: baseURL.appendingPathComponent(path), resolvingAgainstBaseURL: false) else {
+            fatalError("Unable to create URL compoenents")
+            
+        }
+        components.queryItems = parameters.map{
+            URLQueryItem(name: String($0), value: String($1))
+        }
+        guard let url = components.url else {
+            fatalError("Could not get url")
+        }
+        let request : URLRequest = {
+            var request = URLRequest(url: url)
+            request.httpMethod = method.rawValue
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            return request
+        }()
+        return request
+    }
+}
+class ApiService {
+    let baseURL = URL(string: "http://universities.hipolabs.com/")!
+    
+    func send<T: Codable>(apiRequest : ApiRequest) -> Observable<T> {
+        return Observable<T>.create { observer in
+            let request = apiRequest.request(with: self.baseURL)
+            let task = URLSession.shared.dataTask(with: request) {
+                (data, response, error) in
+                
+                do {
+                    let model : T = try JSONDecoder().decode(T.self, from: data ?? Data())
+                    observer.onNext(model)
+                } catch let error {
+                    observer.onError(error)
+                }
+                observer.onCompleted()
+            }
+            task.resume()
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    /**
+     searchController.searchBar.rx.text.asObservable()
+     .map { ($0 ?? "").lowercased() }
+     .map { UniversityRequest(name: $0) }
+     .flatMap { request -> Observable<[UniversityModel]> in
+     return self.apiClient.send(apiRequest: request)
+     }
+     .bind(to: tableView.rx.items(cellIdentifier: cellIdentifier)) { index, model, cell in
+     cell.textLabel?.text = model.name
+     }
+     .disposed(by: disposeBag)
+    **/
+}
