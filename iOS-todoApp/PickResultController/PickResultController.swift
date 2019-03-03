924d9bf8e9c981a788d9ba045cd9ee06ce21cb16
sesang06 <sesang06@naver.com>
2019-03-03 17:55:23 +0900

    제출 뷰

--- iOS-todoApp/PickResultController/PickResultController.swift
+++ iOS-todoApp/PickResultController/PickResultController.swift
@@ -13,28 +13,80 @@ import SnapKit
 import RxCocoa
 import RxViewController
 
+
+fileprivate let sampleData: [Restaurant] = {
+  let dataOne: Restaurant = {
+    var a = Restaurant()
+    a.location = "hello world"
+    a.name = "heelo!!"
+    return a
+  }()
+  let dataTwo: Restaurant = {
+    var a = Restaurant()
+    a.location = "hello world"
+    a.name = "heelo!!"
+    return a
+  }()
+  let dataThree: Restaurant = {
+    var a = Restaurant()
+    a.location = "hello world"
+    a.name = "heelo!!"
+    return a
+  }()
+  return [dataOne, dataTwo, dataThree]
+}()
+
+
 protocol PickResultInput {
   var viewDidLoad : PublishSubject<Void> { get }
   var filter: PublishSubject<Filter> { get }
+  var buttonDidTap : PublishSubject<Void> { get }
 }
 
 protocol PickResultOutput {
-  
+  var result: Driver<Restaurant> { get }
 }
 
-class PickResultViewModel {
+class PickResultViewModel: PickResultInput, PickResultOutput{
+ 
+  
   
   // input
   let viewDidLoad = PublishSubject<Void>()
   
   let filter = PublishSubject<Filter>()
   
+  let buttonDidTap = PublishSubject<Void>()
+  
+  // output
+  var result: Driver<Restaurant>
+  
+  
+  
+  let restaurants = BehaviorRelay<[Restaurant]>(value: [])
+  
+  init(){
+    self.result = Driver<Restaurant>.from(optional: nil)
+  }
+  
   
 }
 
 class PickResultController: BaseViewController {
 
+  let pickButton: UIButton = {
+    let b = UIButton()
+    b.tintColor = .black
+    b.setTitle("뽑기", for: .normal)
+    return b
+  }()
+  
+  let resultLabel: UILabel = {
+    let label = UILabel()
+    label.text = "결과"
+    return label
+  }()
   override func setUpViews() {
-    
+//    rx.viewDidLoad.as
   }
 }
